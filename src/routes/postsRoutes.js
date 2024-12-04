c0bba7f58afcba015853e41d79f082ae47a16520
Lauanderson Rael <100454015+lauanderson-rael@users.noreply.github.com>
2024-12-04 14:46:23 -0300

    Update postsRoutes.js

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,9 +1,9 @@
-import express from 'express';
+import express from 'express'
 import multer from 'multer';
-import cors from 'cors';
+import cors from 'cors'
 import { atualizarNovoPost, deletarPostPorId, listarPosts, postarNovoPost, uploadImagem } from '../controllers/postsController.js';
 
-// Avisar que vamos receber requisições de links externos
+// avisar que vamos receber requisicoes de uma link externo
 const corsOptions = {
     origin: ["http://localhost:8000", "http://localhost:5173", "https://lauanderson-portfolio.vercel.app", "https://backend-instabytes.vercel.app", "https://lauanderson-portfolio-k1baotfie-lauanderson-raels-projects.vercel.app"],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
@@ -11,32 +11,29 @@ const corsOptions = {
     optiosSuccessStatus: 200
 }
 
-// Configuração do multer para upload de imagens
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads/'); // Diretório onde os arquivos serão salvos
+        cb(null, 'uploads/');
+
     },
     filename: function (req, file, cb) {
-        cb(null, file.originalname); // Mantém o nome original do arquivo
+        cb(null, file.originalname);
     }
-});
+})
 
-// Inicializando o multer sem a opção `dest`
-const upload = multer({ storage });
+const upload = multer({ dest: './uploads', storage })
 
 const routes = (app) => {
-    app.use(express.json());
-    app.use(cors(corsOptions));
+    app.use(express.json())
+    app.use(cors(corsOptions))
 
-    // Definindo as rotas
     app.get("/posts", listarPosts);
-    app.post("/posts", postarNovoPost);
-    
-    // Rota para upload de imagem
-    app.post("/upload", upload.single("imagem"), uploadImagem);
+    app.post("/posts", postarNovoPost)
+    app.post("/upload", upload.single("imagem"), uploadImagem)
 
-    app.put("/upload/:id", atualizarNovoPost);
-    app.delete("/delete/:id", deletarPostPorId);
+    app.put("/upload/:id", atualizarNovoPost)
+
+    app.delete("/delete/:id", deletarPostPorId)
 }
 
 export default routes;
