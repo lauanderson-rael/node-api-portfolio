856b5d9137f3d70d9e3677f1243d2af6e3ceef41
Lauanderson Rael <100454015+lauanderson-rael@users.noreply.github.com>
2024-12-05 10:47:05 -0300

    Update postsRoutes.js

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -5,7 +5,7 @@ import { atualizarNovoPost, deletarPostPorId, listarPosts, postarNovoPost, uploa
 
 // avisar que vamos receber requisicoes de uma link externo
 const corsOptions = {
-    origin: ["http://localhost:8000", "http://localhost:5173", "https://lauanderson-portfolio.vercel.app", "https://backend-instabytes.vercel.app", "https://lauanderson-portfolio-k1baotfie-lauanderson-raels-projects.vercel.app"],
+    origin: ["http://localhost:8000", "http://localhost:5173", "https://lauanderson-portfolio.vercel.app", "https://backend-instabytes.vercel.app", "https://lauanderson-portfolio-k1baotfie-lauanderson-raels-projects.vercel.app", "https://backend-blog-tec.onrender.com"],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     optiosSuccessStatus: 200
