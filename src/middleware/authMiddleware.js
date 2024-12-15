36e626f54a7d1b93561c7c3893e6d43aa9f60749
lauanderson-rael <lauanderson38@gmail.com>
2024-12-15 08:39:35 -0300

    ccc

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -5,12 +5,14 @@ dotenv.config();
 
 export default function autenticar(req, res, next) {
     const authHeader = req.headers.authorization;
+    console.log(authHeader)
 
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
         return res.status(401).json({ mensagem: "Token não fornecido ou inválido." });
     }
 
     const token = authHeader.split(" ")[1];
+
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.username = decoded;
